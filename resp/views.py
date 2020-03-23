5df69a50e9f4662f10f7b47e68d988cd7a167571
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-03-23 19:43:48 +0100

    Initial commit - App completada

---
+++ resp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.views.generic import View
+import requests
+import time
+import json
+import webbrowser
+
+class ResponseView(View):
+
+    def get(self, request):
+        if request.GET.get('dominio'):
+            url = 'http://'+request.GET['dominio']
+            destination = url
+            if request.GET.get('ip'):
+                ip = 'http://'+request.GET['ip']
+                destination = ip
+            before = time.time()
+            r = requests.get(destination)
+            responseTime = round((time.time() - before) * 1000)
+            webbrowser.open_new_tab(destination)
+            if r.history:
+                print("Request was redirected")
+                for resp in r.history:
+                    print(resp.status_code, resp.url)
+                    print("Final destination:\n{} {}".format(r.status_code, r.url))
+                rdict = dict(status_code=r.history[0].status_code, time='{}ms'.format(responseTime))
+            else:
+                print("Request was not redirected")
+                rdict = dict(status_code=r.status_code, time='{}ms'.format(responseTime))
+            rjson = json.dumps(rdict)
+            context = {
+                'rjson': rjson,
+                'destination': r.url
+            }
+            print(rjson)
+            return render(request, 'home.html', context)
+        else:
+            return render(request, 'home.html')
